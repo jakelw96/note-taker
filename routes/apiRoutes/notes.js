bce3c3ede21a666ddaaf2b525b105cf8bab0eafa
jakelw96 <jake.lw96@gmail.com>
2021-05-25 22:56:24 -0600

    Created post route to save notes to json file

--- routes/apiRoutes/notes.js
+++ routes/apiRoutes/notes.js
@@ -1,17 +1,25 @@
 const fs = require('fs');
-const path = require('path');
+const createNewNote = require('../../lib');
+const generateUniqueId = require('generate-unique-id');
 const router = require('express').Router();
+const notes = fs.readFileSync('./db/db.json');
 
 // Reads JSON data and returns to page
 router.get('/notes', (req, res) => {
-  const retrievedData = fs.readFileSync('./db/db.json');
-  const noteData = JSON.parse(retrievedData);
+  const noteData = JSON.parse(notes);
   res.json(noteData);
 });
 
 // Receives new note and adds to JSON file then returns
 router.post('/notes', (req, res) => {
+    // Creates unique id for each note in the JSON file
+    req.body.id = generateUniqueId();
     
-}
+    // Add note to json file
+    const newNote = createNewNote(req.body, notes)
+    
+    res.json(newNote)
+});
+
 
 module.exports = router;
