46f34c6db09ec634a537c667f9e8dabf43bda129
jakelw96 <jake.lw96@gmail.com>
2021-05-26 00:27:13 -0600

    Fixed code not appearing on side bar and not writing json correctly

--- routes/apiRoutes/notes.js
+++ routes/apiRoutes/notes.js
@@ -2,21 +2,21 @@ const fs = require('fs');
 const createNewNote = require('../../lib');
 const generateUniqueId = require('generate-unique-id');
 const router = require('express').Router();
-const notes = fs.readFileSync('./db/db.json');
 
 // Reads JSON data and returns to page
 router.get('/notes', (req, res) => {
-  const noteData = JSON.parse(notes);
+  const noteData = JSON.parse(fs.readFileSync('./db/db.json'));
   res.json(noteData);
 });
 
 // Receives new note and adds to JSON file then returns
 router.post('/notes', (req, res) => {
+    let currNotes = fs.readFileSync('./db/db.json')
     // Creates unique id for each note in the JSON file
     req.body.id = generateUniqueId();
     
     // Add note to json file
-    const newNote = createNewNote(req.body, notes)
+    const newNote = createNewNote(req.body, currNotes)
     
     res.json(newNote)
 });
